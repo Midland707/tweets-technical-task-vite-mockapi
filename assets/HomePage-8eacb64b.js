c205ae87411abf440a03f60c7768eb60c20939c5
Midland707 <Midland707@users.noreply.github.com>
2023-04-22 22:46:10 +0000

    deploy: fe38fe62b1fddfaa9fc4232b210dc9680e6b600c

--- assets/HomePage-8eacb64b.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-3d1944fc.js";import{H as r}from"./Helmet-500745a0.js";const o=()=>e.jsxs("main",{children:[e.jsx(r,{children:e.jsx("title",{children:"Home"})}),e.jsx("h2",{children:"My favorite users"})]});export{o as default};
