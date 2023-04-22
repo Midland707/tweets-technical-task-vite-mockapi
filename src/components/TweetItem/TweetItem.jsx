fe38fe62b1fddfaa9fc4232b210dc9680e6b600c
Midland707 <rus14job@ukr.net>
2023-04-23 01:45:24 +0300

    add style to TweetCard

--- src/components/TweetItem/TweetItem.jsx
+++ src/components/TweetItem/TweetItem.jsx
@@ -1,10 +1,35 @@
-import { Item, TweetCard } from "./TweetItem.styled";
+import {
+  Item,
+  TweetCard,
+  Rectangle,
+  CardLogo,
+  CardImage,
+  AvatarBorder,
+  Avatar,
+  Tweets,
+  Followers,
+  TweetBtn,
+} from "./TweetItem.styled";
+import logo from "../../images/logo.svg";
+import image from "../../images/image.png";
 
 export const TweetItem = () => {
   return (
     <Item>
-      <p>TweetItem</p>
-      <TweetCard></TweetCard>
+      <TweetCard>
+        <CardLogo alt="card logo" src={logo} />
+        <CardImage alt="card image" src={image} />
+        <Rectangle />
+        <AvatarBorder>
+          <Avatar
+            alt="avatar"
+            src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/105.jpg"
+          />
+        </AvatarBorder>
+        <Tweets>Tweets</Tweets>
+        <Followers>Followers</Followers>
+        <TweetBtn>Follow</TweetBtn>
+      </TweetCard>
     </Item>
   );
 };
--- src/components/TweetItem/TweetItem.styled.jsx
+++ src/components/TweetItem/TweetItem.styled.jsx
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
-export const Item = styled.li``;
+export const Item = styled.li`
+  list-style: none;
+`;
 
 export const TweetCard = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  z-index: 1;
   width: 380px;
   height: 460px;
   background: linear-gradient(
@@ -13,4 +21,89 @@ export const TweetCard = styled.div`
   );
   box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
   border-radius: 20px;
+`;
+
+export const CardLogo = styled.img`
+  display: block;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  width: 76px;
+  height: 22px;
+  z-index: 3;
+`;
+
+export const CardImage = styled.img`
+  display: block;
+  width: 308px;
+  height: 168px;
+  margin-bottom: 90px;
+  z-index: 2;
+`;
+
+export const Rectangle = styled.div`
+  position: absolute;
+  width: 380px;
+  height: 8px;
+  left: 0px;
+  top: 214px;
+
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+    inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
+`;
+
+export const AvatarBorder = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background: #ebd8ff;
+  box-shadow: 0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06),
+    inset 0px -2.19582px 4.39163px #ae7be3,
+    inset 0px 4.39163px 3.29372px #fbf8ff;
+  overflow: hidden;
+
+  position: absolute;
+  top: 180px;
+  width: 80px;
+  height: 80px;
+`;
+
+export const Avatar = styled.img`
+  display: block;
+  width: 62px;
+  height: 62px;
+  border-radius: 50%;
+  background: #5736a3;
+`;
+
+export const Tweets = styled.h2`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+  color: #ebd8ff;
+  margin: 0 0 16px;
+`;
+export const Followers = styled.h3`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+  color: #ebd8ff;
+  margin: 0 0 26px;
+`;
+export const TweetBtn = styled.button`
+  width: 196px;
+  height: 50px;
+
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10.3108px;
+  margin-bottom: 36px;
 `;
