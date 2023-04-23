061508f1c23edac353c5256d65ddc12820bc10ef
Midland707 <Midland707@users.noreply.github.com>
2023-04-23 10:52:10 +0000

    deploy: 66afe4be3e4d5ad4e6082ea0747e0606853b145f

--- assets/HomePage-c7511ec7.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-b85af83d.js";import{H as r}from"./Helmet-f5ae7629.js";const o=()=>e.jsxs("main",{children:[e.jsx(r,{children:e.jsx("title",{children:"Home"})}),e.jsx("h2",{children:"My favorite users"})]});export{o as default};
---
+++ assets/HomePage-ff18d234.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-b426e5fa.js";import{H as r}from"./Helmet-57ebbceb.js";const o=()=>e.jsxs("main",{children:[e.jsx(r,{children:e.jsx("title",{children:"Home"})}),e.jsx("h2",{children:"My favorite users"})]});export{o as default};
